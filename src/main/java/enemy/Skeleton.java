493c881c59fe212bfc1101ea9c7b749313fd1e7f
Artyom <artyom.tsirsis@icloud.com>
2022-02-05 20:41:43 +0200

    weapon/armor factories added

--- src/main/java/enemy/Skeleton.java
+++ src/main/java/enemy/Skeleton.java
@@ -4,4 +4,8 @@ public class Skeleton extends Enemy {
 
     private final static EnemyClass enemyClass = EnemyClass.Skeleton;
 
+    public Skeleton(EnemyClass enemyClass, Integer health, Integer dexterity, Integer physicalDamage, Integer magicalDamage, Integer physicalDefense, Integer magicalDefense) {
+        super(enemyClass, health, dexterity, physicalDamage, magicalDamage, physicalDefense, magicalDefense);
+    }
+
 }
--- src/main/java/enemy/Spider.java
+++ src/main/java/enemy/Spider.java
@@ -5,4 +5,8 @@ public class Spider extends Enemy {
     private final static EnemyClass enemyClass = EnemyClass.Spider;
 
 
+    public Spider(EnemyClass enemyClass, Integer health, Integer dexterity, Integer physicalDamage, Integer magicalDamage, Integer physicalDefense, Integer magicalDefense) {
+        super(enemyClass, health, dexterity, physicalDamage, magicalDamage, physicalDefense, magicalDefense);
+    }
+
 }
