699ed223c6c988ce4cdb91da62e53013393f6b50
Artyom <artyom.tsirsis@icloud.com>
2022-02-07 17:53:01 +0200

    Main cleaned

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,8 @@
-import hero.HeroClass;
-import hero.HeroRepository;
-import hero.Knight;
-import menu.MainMenu;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        HeroRepository.heroRepo.put(HeroClass.KNIGHT, new Knight("Arthur"));
-        System.out.println("Welcome you, adventurer!");
-        System.out.println(HeroRepository.heroRepo.get(HeroClass.KNIGHT));
-        new MainMenu().openMenu();
+
 
     }
 
