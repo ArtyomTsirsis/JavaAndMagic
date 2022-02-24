e34f3f1bb6131701c21cad5d6fe928e737b7b47e
nextlevelplay <sweer27@gmail.com>
2022-02-24 16:14:58 +0200

    LazyMain remove comments

--- src/main/java/battle/InnerLazyMain.java
+++ src/main/java/battle/InnerLazyMain.java
@@ -7,9 +7,6 @@ public class InnerLazyMain {
 
     public static void main(String[] args) {
 
-        // Локальная визуализаторная, у Паши в одном из UI сбой и я хз почему, просто удалить временно его комент -
-        // ( //    Long id; надо переделать на ИД)
-
         Battle battle = new Battle(new Knight("Rex"), new Orc());
         battle.fightSimulator();
     }
