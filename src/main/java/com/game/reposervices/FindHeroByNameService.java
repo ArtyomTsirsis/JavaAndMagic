3532a8dd2c28d63a6a2c6ff6bfbe3d75a20688b7
Artyom <artyom.tsirsis@icloud.com>
2022-03-25 19:29:34 +0200

    FindByNameService fix

--- src/main/java/com/game/reposervices/FindHeroByNameService.java
+++ src/main/java/com/game/reposervices/FindHeroByNameService.java
@@ -1,10 +1,14 @@
 package com.game.reposervices;
 
-import com.game.utils.ConverterToDto;
 import com.game.dto.FindHeroByIdResponse;
+import com.game.repository.hero.Hero;
 import com.game.repository.hero.HeroRepository;
+import com.game.utils.ConverterToDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.rmi.NoSuchObjectException;
+import java.util.Optional;
 
 @Component
 public class FindHeroByNameService {
@@ -14,7 +18,13 @@ public class FindHeroByNameService {
     @Autowired
     private ConverterToDto converter;
 
-    public FindHeroByIdResponse findByName(String name) {
-        return new FindHeroByIdResponse(converter.convertToDto(repository.findById(name)));
+    public FindHeroByIdResponse findByName(String name) throws NoSuchObjectException {
+        Optional<Hero> hero = repository.findById(name);
+        if (hero.isEmpty()) {
+            throw new NoSuchObjectException(name);
+
+        }
+        return new FindHeroByIdResponse(converter.convertToDto(hero.get()));
+
     }
 }
