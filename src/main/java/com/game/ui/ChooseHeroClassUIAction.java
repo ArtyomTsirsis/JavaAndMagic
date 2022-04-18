3efbd3c8971906cdcfe88539fc069339622d12fd
nextlevelplay <sweer27@gmail.com>
2022-04-18 22:17:16 +0300

    add ChooseHeroClassUIAction

---
+++ src/main/java/com/game/ui/ChooseHeroClassUIAction.java
@@ -0,0 +1,8 @@
+package com.game.ui;
+
+public class ChooseHeroClassUIAction implements UIAction{
+    @Override
+    public void execute() {
+
+    }
+}
--- src/main/java/com/game/ui/CreateHeroUIAction.java
+++ src/main/java/com/game/ui/CreateHeroUIAction.java
@@ -22,25 +22,72 @@ public class CreateHeroUIAction implements UIAction {
 
     public void execute() {
 
-        System.out.println("\033[H\033[2J");
-        System.out.flush();
-        // запрашиваем данные о герое с консоли у пользолвателя (пока только имя и класс =))
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Please, enter Hero name: ");
-        heroName = sc.nextLine();
+        var scanner = new Scanner(System.in);
 
-        System.out.println("**************************************");
-        System.out.println("We made a simple four classes!");
+        System.out.println("       █████                                      █████████                  █████    ██████   ██████                     ███          \n" +
+                "      ░░███                                      ███░░░░░███                ░░███    ░░██████ ██████                     ░░░           \n" +
+                "       ░███   ██████   █████ █████  ██████      ░███    ░███  ████████    ███████     ░███░█████░███   ██████    ███████ ████   ██████ \n" +
+                "       ░███  ░░░░░███ ░░███ ░░███  ░░░░░███     ░███████████ ░░███░░███  ███░░███     ░███░░███ ░███  ░░░░░███  ███░░███░░███  ███░░███\n" +
+                "       ░███   ███████  ░███  ░███   ███████     ░███░░░░░███  ░███ ░███ ░███ ░███     ░███ ░░░  ░███   ███████ ░███ ░███ ░███ ░███ ░░░ \n" +
+                " ███   ░███  ███░░███  ░░███ ███   ███░░███     ░███    ░███  ░███ ░███ ░███ ░███     ░███      ░███  ███░░███ ░███ ░███ ░███ ░███  ███\n" +
+                "░░████████  ░░████████  ░░█████   ░░████████    █████   █████ ████ █████░░████████    █████     █████░░████████░░███████ █████░░██████ \n" +
+                " ░░░░░░░░    ░░░░░░░░    ░░░░░     ░░░░░░░░    ░░░░░   ░░░░░ ░░░░ ░░░░░  ░░░░░░░░    ░░░░░     ░░░░░  ░░░░░░░░  ░░░░░███░░░░░  ░░░░░░  \n" +
+                "                                                                                                                ███ ░███               \n" +
+                "                                             Console microgame by ComboBreakers team.                          ░░██████                \n" +
+                "                                                                                                                ░░░░░░                 ");
+
         System.out.println();
-        System.out.println("1) Archer. (accurate shooter, use a bow like a main weapon.)");
-        System.out.println("2) Knight. (Dressed in shiny armor, with a two-handed sword.)");
-        System.out.println("3) Wizard. (A hooded robe hides his 'dark' eyes as night.)");
-        System.out.println("4) Thief. (Thunderstorm marketplaces, take care of your wallets! Armed with 2 daggers.)");
+        System.out.println("======================================================================================================================================");
+        System.out.println("|                                                        CREATE HERO                                                                 |");
+        System.out.println("======================================================================================================================================");
+        System.out.println("|                                                                                                                                    |");
+        System.out.println("|                                    `,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`                                         |");
+        System.out.println("|                                    `,`                 _                  _+       |   `,`                                         |");
+        System.out.println("|                                    `,`                / `----------------' |  \\+/  |   `,`                                         |");
+        System.out.println("|                                    `,`                \\_|     WELCOME!   |_| _<=>_ |   `,`                                         |");
+        System.out.println("|                                    `,`                  `----------------' 0/ \\ / o=o  `,`                                         |");
+        System.out.println("|                                    `,`                                     \\_\\ ^ /`0   `,`                                         |");
+        System.out.println("|                                    `,`  PLEASE, ENTER HERO NAME:           | /_^_\\     `,`                                         |");
+        System.out.println("|                                    `,`                                     | || ||     `,`                                         |");
+        System.out.println("|                                    `,`_____________________________________|_d|_|b____ `,`                                         |");
+        System.out.println("|                                    `,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`                                         |");
+        System.out.println("|                                                                                                                                    |");
+        System.out.println("|                                                                                                                                    |");
+        System.out.println("|                                                                                                                                    |");
+        System.out.println("======================================================================================================================================");
+        System.out.print("Please, enter Hero name ⮞ ");
+
+        heroName = scanner.nextLine();
+
+        System.out.println("       █████                                      █████████                  █████    ██████   ██████                     ███          \n" +
+                "      ░░███                                      ███░░░░░███                ░░███    ░░██████ ██████                     ░░░           \n" +
+                "       ░███   ██████   █████ █████  ██████      ░███    ░███  ████████    ███████     ░███░█████░███   ██████    ███████ ████   ██████ \n" +
+                "       ░███  ░░░░░███ ░░███ ░░███  ░░░░░███     ░███████████ ░░███░░███  ███░░███     ░███░░███ ░███  ░░░░░███  ███░░███░░███  ███░░███\n" +
+                "       ░███   ███████  ░███  ░███   ███████     ░███░░░░░███  ░███ ░███ ░███ ░███     ░███ ░░░  ░███   ███████ ░███ ░███ ░███ ░███ ░░░ \n" +
+                " ███   ░███  ███░░███  ░░███ ███   ███░░███     ░███    ░███  ░███ ░███ ░███ ░███     ░███      ░███  ███░░███ ░███ ░███ ░███ ░███  ███\n" +
+                "░░████████  ░░████████  ░░█████   ░░████████    █████   █████ ████ █████░░████████    █████     █████░░████████░░███████ █████░░██████ \n" +
+                " ░░░░░░░░    ░░░░░░░░    ░░░░░     ░░░░░░░░    ░░░░░   ░░░░░ ░░░░ ░░░░░  ░░░░░░░░    ░░░░░     ░░░░░  ░░░░░░░░  ░░░░░███░░░░░  ░░░░░░  \n" +
+                "                                                                                                                ███ ░███               \n" +
+                "                                             Console microgame by ComboBreakers team.                          ░░██████                \n" +
+                "                                                                                                                ░░░░░░                 ");
+
         System.out.println();
-        System.out.print("What your choice?: ");
+        System.out.println("======================================================================================================================================");
+        System.out.println("|                                                        CHOOSE HERO CLASS                                                           |");
+        System.out.println("======================================================================================================================================");
+        System.out.println("|                                                                                                                                    |");
+        System.out.println("|                                                 We made a simple four classes!                                                     |");
+        System.out.println("|                                                                                                                                    |");
+        System.out.println("|                                   1. Archer (accurate shooter, use a bow like a main weapon.)                                      |");
+        System.out.println("|                                   2. Knight (Dressed in shiny armor, with a two-handed sword.)                                     |");
+        System.out.println("|                                   3. Wizard (A hooded robe hides his 'dark' eyes as night.)                                        |");
+        System.out.println("|                                   4. Thief  (Thunderstorm marketplaces, take care of your wallets! Armed with 2 daggers.)          |");
+        System.out.println("|                                                                                                                                    |");
+        System.out.println("|                                                                                                                                    |");
+        System.out.println("======================================================================================================================================");
+        System.out.print("Please make a choice ⮞ ");
 
-
-        int selector = Integer.parseInt(sc.nextLine());
+        int selector = Integer.parseInt(scanner.nextLine());
         switch (selector) {
             case 1 -> heroClass = HeroClass.ARCHER;
             case 2 -> heroClass = HeroClass.KNIGHT;
@@ -63,3 +110,4 @@ public class CreateHeroUIAction implements UIAction {
     }
 
 }
+
