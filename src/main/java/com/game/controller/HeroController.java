05cdafeb207119157e3e3ef4c08366fb786a96c8
Artyom <artyom.tsirsis@icloud.com>
2022-04-30 14:32:24 +0300

    Sorry. Too many changes and refactor. was needed to fix DB bug with sessions.

--- src/main/java/com/game/controller/HeroController.java
+++ src/main/java/com/game/controller/HeroController.java
@@ -1,23 +1,15 @@
 package com.game.controller;
 
-import com.game.dto.armor.CreateArmorRequest;
-import com.game.dto.armor.FindArmorByIdResponse;
 import com.game.dto.hero.CreateHeroRequest;
 import com.game.dto.hero.FindAllHeroesResponse;
 import com.game.dto.hero.FindHeroByIdResponse;
-import com.game.dto.hero.HeroDTO;
-import com.game.reposervices.armor.CreateArmorService;
-import com.game.reposervices.armor.FindArmorByIDService;
 import com.game.reposervices.hero.CreateHeroService;
+import com.game.reposervices.hero.DeleteHeroByNameService;
 import com.game.reposervices.hero.FindAllHeroesService;
 import com.game.reposervices.hero.FindHeroByNameService;
-import com.game.repository.armor.ArmorClass;
 import com.game.repository.hero.HeroClass;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.rmi.NoSuchObjectException;
 
@@ -29,34 +21,26 @@ public class HeroController {
     private final CreateHeroService createHeroService;
     private final FindHeroByNameService findHeroByNameService;
     private final FindAllHeroesService findAllHeroesService;
-    private final CreateArmorService createArmorService;
-    private final FindArmorByIDService findArmorByIDService;
+    private final DeleteHeroByNameService deleteHeroByNameService;
 
-    @GetMapping(value = "/hero")
-    public void createHero() {
-        createHeroService.createHero(new CreateHeroRequest(HeroClass.KNIGHT, "Maxwell"));
+    @PutMapping(value = "/{name}")
+    public void createHero(@PathVariable String name) {
+        createHeroService.createHero(new CreateHeroRequest(HeroClass.KNIGHT, name));
     }
 
-    @GetMapping(value = "hero/{name}")
-    public FindHeroByIdResponse getArthur(@PathVariable String name) throws NoSuchObjectException {
+    @GetMapping(value = "/{name}")
+    public FindHeroByIdResponse getHero(@PathVariable String name) throws NoSuchObjectException {
         return findHeroByNameService.findByName(name);
     }
 
-    @GetMapping(value = "heroes")
+    @DeleteMapping(value = "/{name}")
+    public void deleteHero(@PathVariable String name) throws NoSuchObjectException {
+        deleteHeroByNameService.deleteByName(name);
+    }
+
+    @GetMapping(value = "/")
     public FindAllHeroesResponse findAllHeroes() {
         return findAllHeroesService.findAll();
-    }
-
-    @GetMapping(value = "armor")
-    public void createArmor() {
-        HeroDTO hero = new HeroDTO();
-        hero.setName("Arthur");
-        createArmorService.createArmor(new CreateArmorRequest(ArmorClass.WITHOUT_ARMOR, new HeroDTO()));
-    }
-
-    @GetMapping(value = "armor/{id}")
-    public FindArmorByIdResponse getArmor(@PathVariable Integer id) throws NoSuchObjectException {
-        return findArmorByIDService.findByID(id);
     }
 
 }
