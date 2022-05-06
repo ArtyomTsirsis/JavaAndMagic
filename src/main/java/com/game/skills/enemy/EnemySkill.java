dd2864436da10f34db79b478027543214dfeb400
Artyom <artyom.tsirsis@icloud.com>
2022-05-06 20:00:47 +0300

    Skill implementation mapping from json fixing

--- src/main/java/com/game/skills/enemy/EnemySkill.java
+++ src/main/java/com/game/skills/enemy/EnemySkill.java
@@ -1,9 +1,20 @@
 package com.game.skills.enemy;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.game.dto.hero.HeroDTO;
 import com.game.repository.enemy.Enemy;
 
 public interface EnemySkill {
+
+    @JsonCreator
+    public static EnemySkill create(@JsonProperty("name") String name) {
+        return switch (name) {
+            case "FireDamage" -> new ConcreteEnemySkillFireDamage();
+            case "PowerAttack" -> new ConcreteEnemySkillPowerAttack();
+            default -> new ConcreteEnemySkillFastAttack();
+        };
+    }
 
     String execute(HeroDTO hero, Enemy enemy);
     String getName();
