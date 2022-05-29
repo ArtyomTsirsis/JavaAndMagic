0a2fcfd0374d8e46baf1756fe96b09aa40be6ae7
Denteron <redisbad11@gmail.com>
2022-05-29 23:13:38 +0300

    hero services tests/

--- src/test/java/com/game/core/repo/hero/UpdateHeroServiceTest.java
+++ src/test/java/com/game/core/repo/hero/UpdateHeroServiceTest.java
@@ -1,7 +1,58 @@
 package com.game.core.repo.hero;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.game.domain.hero.Hero;
+import com.game.dto.hero.HeroDTO;
+import com.game.dto.hero.UpdateHeroRequest;
+import com.game.repository.HeroRepository;
+import com.game.utils.HeroDTOConverter;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class UpdateHeroServiceTest {
+
+    @Mock
+    private HeroRepository repository;
+
+    @Mock
+    private HeroDTOConverter converter;
+
+    @InjectMocks
+    private UpdateHeroService heroService;
+
+    @Test
+    void shouldFindHeroByName() {
+        when(repository.findById("TEST NAME")).thenReturn(Optional.of(hero()));
+        when(converter.convertFromDto(heroDTO())).thenReturn(hero());
+        heroService.update(request());
+        verify(repository).update(hero());
+    }
+
+    private UpdateHeroRequest request() {
+        var request = new UpdateHeroRequest();
+        request.setUpdatedHeroDTO(heroDTO());
+        return request;
+    }
+
+    private Hero hero() {
+        var hero = new Hero();
+        hero.setName("TEST NAME");
+        return hero;
+    }
+
+    private HeroDTO heroDTO() {
+        var heroDto = new HeroDTO();
+        heroDto.setName("TEST NAME");
+        return heroDto;
+    }
+
 
 }
